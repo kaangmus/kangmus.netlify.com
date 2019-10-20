25730b80bffe6b3e7bd35995b0066a5eacbf370d
Musliadi <myblogg1924@gmail.com>
2019-10-20 10:50:59 +0700

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -53,12 +53,14 @@ const Bio = () => {
           borderRadius: `50%`,
         }}
       />
+      <div>
       <p> Musliadi <strong>{author}</strong> Mahasiswa Asal Aceh Yang Merantau di Jogja, Menulis salah satu dari hobi saya <br>
          <a href={`https://github.com/msmusli`}>Github</a> |
         <a href={`https://facebook.com/${social.facebook}`}>
           Yuk Berteman di FB
         </a>
       </p>
+    </div>
     </div>
   )
 }
